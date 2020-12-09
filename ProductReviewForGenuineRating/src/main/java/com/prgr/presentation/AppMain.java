2cc8d24528c5af5b03b76b216520c121f740b8ec
Prathamesh Prabhu <prathamesh.prabhu@capgemini.com>
2020-12-09 14:31:36 +0530

    Appman class changed.

--- ProductReviewForGenuineRating/src/main/java/com/prgr/presentation/AppMain.java
+++ ProductReviewForGenuineRating/src/main/java/com/prgr/presentation/AppMain.java
@@ -27,6 +27,7 @@ public class AppMain {
 			int adminOption=option.nextInt();
 			switch(adminOption) {
 			/* ---------------------Admin Page----------------------- */
+			/* ---------------------Admin Page----------------------- */
 					
 			case 1:
 					System.out.println("Admin login Page");
